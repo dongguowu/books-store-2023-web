0b662322563d4f8b9b3a4d03972f8bddde1378a0
David WU <david.dong.guo@gmail.com>
2023-01-19 16:52:56 -0500

    add function addCart

---
+++ js/functions.js
@@ -0,0 +1,13 @@
+function addCart(value) {
+  var element = document.getElementById("current-cart");
+  var array = element.innerHTML.split("(");
+  var currentValue = 0;
+  if (array.length >= 2) {
+    var subArray = array[1].split(")");
+    if (subArray.length >= 2) {
+      currentValue = parseInt(subArray[0], 10);
+    }
+  }
+  currentValue += parseInt(value);
+  element.innerHTML = "Cart(" + currentValue + ")";
+}
