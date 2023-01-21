31e1f441aba3155a875d055438b7ebdd6ae9a384
David WU <david.dong.guo@gmail.com>
2023-01-20 23:31:14 -0500

    finish the function of editing cart

--- js/functions.js
+++ js/functions.js
@@ -15,3 +15,33 @@ function addCart(value) {
     cartButton.classList.remove("disabled");
   }
 }
+
+function getItemQuantity(item) {
+  return parseInt(item.find(".item-quantity").val());
+}
+
+function calculateItemTotal(item) {
+  var price = item.find(".item-price").text().slice(1);
+  var quantity = item.find(".item-quantity").val();
+  var itemTotal = (price * quantity).toFixed(2);
+  item.find(".item-total").text("$" + itemTotal);
+}
+
+// function to calculate the overall total
+function calculateTotal() {
+  var quantityOfItems = 0;
+  $(".shopping-cart-line").each(function () {
+    quantityOfItems += getItemQuantity($(this));
+  });
+
+  var total = 0;
+  $(".shopping-cart-line").each(function () {
+    calculateItemTotal($(this));
+    var itemTotal = $(this).find(".item-total").text().slice(1);
+    total += parseFloat(itemTotal);
+  });
+
+  $("#order-total").text(
+    "items(" + quantityOfItems + "): $" + total.toFixed(2)
+  );
+}
